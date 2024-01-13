0be0b2a6de23b2b5c72e1f9b7410a8802ea3247f
Nehemiah <njgdnb@gmail.com>
2024-01-13 12:09:28 -0700

    Added cart & login endpoint. Changed home button to reload homepage. Added filler code into catalog, cart, & login page.

--- routes/catalog.js
+++ routes/catalog.js
@@ -6,7 +6,8 @@ const pool = mariadb.createPool({
     host: 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'fzsjdmm68733hgu4',
     password: 'kq86u525do1qwod4',
-    database: 'f3uipf8n2hwoxtt3'
+    database: 'f3uipf8n2hwoxtt3',
+    connectionLimit: 1
 });
 
 /* GET catalog listing. */
