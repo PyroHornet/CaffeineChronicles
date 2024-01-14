30ea71a5a8dc32c439755cefd2bd0cf050a2b5fc
PyroHornet <32351468+PyroHornet@users.noreply.github.com>
2024-01-14 14:35:34 -0500

    trying to see if changing Pool to featuredPool does anything, maybe there is a variable conflict?

--- routes/homepage.js
+++ routes/homepage.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const mariadb = require('mariadb');
 
-const pool = mariadb.createPool({
+const featuredPool = mariadb.createPool({
   host: 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user: 'fzsjdmm68733hgu4',
   password: 'kq86u525do1qwod4',
@@ -15,8 +15,8 @@ router.get('/', async function (req, res, next) {
   //res.render('homepage', { title: 'Express' });
   let conn;
   try {
-    conn = await pool.getConnection();
-    const rows = await conn.query('SELECT * FROM Books WHERE IsFeatured = 1');
+    conn = await featuredPool.getConnection();
+    const rows = await conn.query("SELECT * FROM Books WHERE IsFeatured IS TRUE");
     res.render('homepage', { books: rows });
   } catch (err) {
     console.error(err);
