7b062b5ed7243074850445ef83a632260d419744
Nehemiah <njgdnb@gmail.com>
2024-01-14 18:10:25 -0700

    Added about page & login page with styling added to cart & about & login

--- routes/contact.js
+++ routes/contact.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     //res.send('respond with a resource');
-    res.send("This is the contact page.")
+    res.render("contact")
 });
 
 module.exports = router;
